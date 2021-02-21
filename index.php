a5821e317523bc3bd37feffa8fd25316eee3234f
Víctor Ponz <victorponz@gmail.com>
2021-02-21 19:37:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfig('client_secrets.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive = new Google_Service_Drive($client);
+  $files = $drive->files->listFiles()->getFiles();
+  print_r($client);
+  foreach($files as $file){
+  	//echo $file['name'] . "-" . $file['mimeType'] . "<br>";
+  	//print_r($file);
+  }
+  
+} else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
