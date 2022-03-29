ca47af537455f6dc6b692ea907163da03041750a
Víctor Ponz Artero <35145182+victorponz@users.noreply.github.com>
2022-03-29 15:50:25 +0200

    Actualizar servidor

--- index.php
+++ index.php
@@ -16,6 +16,6 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   }
   
 } else {
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  $redirect_uri = 'http://localhost:8080/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
